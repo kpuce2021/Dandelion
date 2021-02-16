a53e64537aac0289573d75665b88fdb636292138
Sang Hyeok Han <contea95@naver.com>
2021-02-16 08:26:33 +0000

    이미지부풀리기수정

--- server/Dandelion-Server/keras_image_generator.py
+++ server/Dandelion-Server/keras_image_generator.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import numpy as np
 import os
@@ -5,30 +6,31 @@ from os import listdir
 from os.path import isfile, join
 from PIL import Image
 
-np.random.seed(3)
+with tf.device('/gpu:0'):
+    np.random.seed(3)
 
 
-data_datagen = ImageDataGenerator(rescale=1./255)
+    data_datagen = ImageDataGenerator(rescale=1./255)
 
-data_datagen = ImageDataGenerator(rescale=1./255, rotation_range=30, shear_range=5.5,
-                                  zoom_range=0., horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
+    data_datagen = ImageDataGenerator(rescale=1./255, rotation_range=30, shear_range=5.5,
+                                      zoom_range=0., horizontal_flip=False, vertical_flip=False, fill_mode='nearest')
 
 
-filename_in_dir = []
+    filename_in_dir = []
 
-for root, dirs, files in os.walk('/home/starshan95/Dandelion/Dataset/Pepero_Original'):
-    for fname in files:
-        full_fname = os.path.join(root, fname)
-        filename_in_dir.append(full_fname)
+    for root, dirs, files in os.walk('/root/Dandelion/Dataset/Pepero_Original'):
+        for fname in files:
+            full_fname = os.path.join(root, fname)
+            filename_in_dir.append(full_fname)
 
-for file_image in filename_in_dir:
-    # print(file_image)
-    img = load_img(file_image)
-    x = img_to_array(img)
-    x = x.reshape((1,) + x.shape)
+    for file_image in filename_in_dir:
+        print(file_image)
+        img = load_img(file_image)
+        x = img_to_array(img)
+        x = x.reshape((1,) + x.shape)
 
-    i = 0
-    for batch in data_datagen.flow(x, save_to_dir='/home/starshan95/Dandelion/Dataset_append', save_prefix='Pepero_Original', save_format='jpg'):
-        i += 1
-        if i > 3:
-            break
+        i = 0
+        for batch in data_datagen.flow(x, save_to_dir='/root/Dandelion/Dataset_append', save_prefix='Pepero_Original', save_format='jpg'):
+            i += 1
+            if i > 3:
+                break
