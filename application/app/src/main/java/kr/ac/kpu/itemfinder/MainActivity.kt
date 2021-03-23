a1b97a28e4ba40f26617e06b5b4b799778324754
Chae Min <limkim4120@naver.com>
2021-03-24 01:35:19 +0900

    안드로이드
    - 코드 정리
    - Toast 메시지로 출력하던 기타 정보들을 Log로 출력할 수 있도록 변경

--- application/app/src/main/java/kr/ac/kpu/itemfinder/MainActivity.kt
+++ application/app/src/main/java/kr/ac/kpu/itemfinder/MainActivity.kt
@@ -35,18 +35,19 @@ import java.io.FileOutputStream
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-
-typealias LumaListener = (luma: Double) -> Unit
-
 class MainActivity : AppCompatActivity() {
+    // CameraX 관련 변수
     private var imageCapture: ImageCapture? = null
-
-    private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    /*
+    // ML Kit 관련 변수
     private lateinit var localModel: LocalModel
     private lateinit var customImageLabelerOptions: CustomImageLabelerOptions
     private lateinit var labeler: ImageLabeler
+     */
 
+    // Retrofit 관련 변수
     private lateinit var retrofit : Retrofit
     private lateinit var retrofitService : RetrofitService
 
@@ -58,22 +59,17 @@ class MainActivity : AppCompatActivity() {
         if (allPermissionsGranted()) {
             startCamera()
         } else {
-            ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-            )
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        help_button.setOnClickListener {
+            val intent = Intent(baseContext, HelpActivity::class.java)
+            startActivity(intent)
         }
 
         // Set up the listener for take photo button
         camera_capture_button.setOnClickListener { takePhoto() }
 
-        help_button.setOnClickListener {
-            Toast.makeText(baseContext, "help_button clicked", Toast.LENGTH_SHORT).show()
-            val intent = Intent(baseContext, HelpActivity::class.java)
-            startActivity(intent)
-        }
-
-        //outputDirectory = getOutputDirectory()
-        outputDirectory = cacheDir
         cameraExecutor = Executors.newSingleThreadExecutor()
 
         /*
@@ -91,103 +87,6 @@ class MainActivity : AppCompatActivity() {
 
         // Retrofit
         initRetrofit()
-
-    }
-
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        // Create time-stamped output file to hold the image
-        val photoFile = File(outputDirectory, "temp.jpg")
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-                outputOptions,
-                ContextCompat.getMainExecutor(this),
-                object : ImageCapture.OnImageSavedCallback {
-                    override fun onError(exc: ImageCaptureException) {
-                        Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                    }
-
-                    // 성공한 경우 업로드 진행
-                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                        val savedUri = Uri.fromFile(photoFile)
-                        val msg = "Photo capture succeeded: $savedUri"
-
-                        /*
-                    val image: InputImage
-                    try {
-                        image = InputImage.fromFilePath(this@MainActivity, savedUri)
-                        labeler.process(image)
-                                .addOnSuccessListener { labels ->
-                                    // Task completed successfully
-                                    if(labels.isEmpty()) {
-                                        Toast.makeText(baseContext, "labeler.process Fail", Toast.LENGTH_SHORT).show()
-                                    }else {
-                                        Toast.makeText(baseContext, "labeler.process Success\n${labels[0].text}\n${labels[0].confidence}", Toast.LENGTH_SHORT).show()
-                                        val intent = Intent(baseContext, ResultActivity::class.java)
-                                        intent.putExtra("product_name", labels[0].text)
-                                        startActivity(intent)
-                                    }
-                                }
-                                .addOnFailureListener { e ->
-                                    // Task failed with an exception
-                                    Toast.makeText(baseContext, "labeler.process Fail\n${e.message}", Toast.LENGTH_SHORT).show()
-                                }
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                     */
-
-                        //val requestBody: RequestBody = RequestBody.create(MediaType.parse("image/*"), photoFile)
-                        //val body: MultipartBody.Part = MultipartBody.Part.createFormData("image", "temp.jpg", requestBody)
-                        //getProductInfo(retrofitService, body)
-
-                        resize(savedUri)
-                    }
-                })
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                    .build()
-                    .also {
-                        it.setSurfaceProvider(viewFinder.createSurfaceProvider())
-                    }
-
-            imageCapture = ImageCapture.Builder().build()
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onDestroy() {
@@ -201,11 +100,11 @@ class MainActivity : AppCompatActivity() {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA, Manifest.permission.INTERNET)
     }
 
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<String>,
-            grantResults: IntArray
-    ) {
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 startCamera()
@@ -214,6 +113,85 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(baseContext)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(cacheDir, "temp.jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                    }
+
+                    // 성공한 경우 업로드 진행
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        val savedUri = Uri.fromFile(photoFile)
+
+                        /*
+                        // ML Kit 관련 코드
+                        val image: InputImage
+                        try {
+                            image = InputImage.fromFilePath(this@MainActivity, savedUri)
+                            labeler.process(image).addOnSuccessListener { labels ->
+                                // Task completed successfully
+                                if(labels.isEmpty()) {
+                                    Toast.makeText(baseContext, "labeler.process Fail", Toast.LENGTH_SHORT).show()
+                                }else {
+                                    Toast.makeText(baseContext, "labeler.process Success\n${labels[0].text}\n${labels[0].confidence}", Toast.LENGTH_SHORT).show()
+                                    val intent = Intent(baseContext, ResultActivity::class.java)
+                                    intent.putExtra("product_name", labels[0].text)
+                                    startActivity(intent)
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                // Task failed with an exception
+                                Toast.makeText(baseContext, "labeler.process Fail\n${e.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                         */
+
+                        resize(savedUri)
+                    }
+                })
     }
 
     private fun initRetrofit() {
@@ -237,28 +215,27 @@ class MainActivity : AppCompatActivity() {
         Picasso.get().load(imgUri).resize(224, 224).into(object : com.squareup.picasso.Target {
             override fun onBitmapLoaded(bitmap: Bitmap?, from: LoadedFrom?) {
                 try {
-                    val resize_file: File = File(outputDirectory, "resize_temp.jpg")
-                    resize_file.createNewFile()
-                    val osstream: FileOutputStream = FileOutputStream(resize_file)
-                    bitmap?.compress(Bitmap.CompressFormat.JPEG, 80, osstream)
-                    osstream.flush()
-                    osstream.close()
-                    val requestBody: RequestBody = RequestBody.create(MediaType.parse("image/*"), resize_file)
+                    val resizeImage = File(cacheDir, "resize_temp.jpg")
+                    resizeImage.createNewFile()
+                    val fileOutputStream = FileOutputStream(resizeImage)
+                    bitmap?.compress(Bitmap.CompressFormat.JPEG, 80, fileOutputStream)
+                    fileOutputStream.flush()
+                    fileOutputStream.close()
+
+                    val requestBody: RequestBody = RequestBody.create(MediaType.parse("image/*"), resizeImage)
                     val body: MultipartBody.Part = MultipartBody.Part.createFormData("image", "temp.jpg", requestBody)
                     getProductInfo(retrofitService, body)
                 } catch (e: java.lang.Exception) {
                     e.printStackTrace()
-                    Toast.makeText(baseContext, "resize_temp.jpg load failed", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onBitmapFailed(e: java.lang.Exception?, errorDrawable: Drawable?) {
-                Toast.makeText(baseContext, "Picasso.resize failed", Toast.LENGTH_SHORT).show()
+                e?.printStackTrace()
             }
 
             override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
             }
-
         })
     }
 }
