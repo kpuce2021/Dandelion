68145ba0bc9ce08056d7591b2fc72fd76afb2da3
Chae Min <limkim4120@naver.com>
2021-04-02 19:26:40 +0900

    안드로이드
    - 이미지 갱신 되지 않는 오류 해결
    - 이미지 회전에 대한 오류 해결 필요

--- application/app/src/main/java/kr/ac/kpu/itemfinder/ResultActivity.kt
+++ application/app/src/main/java/kr/ac/kpu/itemfinder/ResultActivity.kt
@@ -2,17 +2,22 @@ package kr.ac.kpu.itemfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 import kotlinx.android.synthetic.main.activity_result.*
 import java.io.File
 
 class ResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         product_name_textview.text = intent.getStringExtra("product_name")+"\n"+intent.getStringExtra("product_confidence")
-        Glide.with(this).load(File(cacheDir, "resize.jpg")).diskCacheStrategy(DiskCacheStrategy.NONE).into(product_img_imageview)
+        val options = RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
+        Glide.with(this).load(File(cacheDir, "resize.jpg")).apply(options).into(product_img_imageview)
     }
 }
