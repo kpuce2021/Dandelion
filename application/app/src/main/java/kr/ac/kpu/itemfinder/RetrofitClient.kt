d484e37a1e5f1e8f3a13efabd17d669ccd5e613a
dasoopark <ektn0514@gmail.com>
2021-05-10 23:27:53 +0900

    retrofit
    retrofit Yolo 기반 JSON설정

--- application/app/src/main/java/kr/ac/kpu/itemfinder/RetrofitClient.kt
+++ application/app/src/main/java/kr/ac/kpu/itemfinder/RetrofitClient.kt
@@ -9,8 +9,7 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
 import com.google.gson.GsonBuilder
 import okhttp3.MultipartBody
-import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.OkHttpClient
+
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,8 +25,6 @@ object RetrofitClient {
     // 서버 주소
     private const val BASE_URL = "http://59.14.252.2:5000/"
 
-
-
     // SingleTon
     fun getInstance(): Retrofit {
         if (instance == null) {
@@ -41,33 +38,21 @@ object RetrofitClient {
     }
 
     fun getProductInfo(context: Context, service: RetrofitService, body: MultipartBody.Part) {
-        var interceptor = HttpLoggingInterceptor()
-        if(BuildConfig.DEBUG){
-            interceptor.level=HttpLoggingInterceptor.Level.BODY
+        service.productPredict(body).enqueue(object : Callback<List<ProductVO>> {
 
-        }else{
-            interceptor.level=HttpLoggingInterceptor.Level.NONE
-        }
-        val client = OkHttpClient().newBuilder()
-                .addNetworkInterceptor(interceptor)
-                .build()
-        service.productPredict(body).enqueue(object : Callback<ProductVO> {
 
-            override fun onFailure(call: Call<ProductVO>, t: Throwable) {
+            override fun onFailure(call: Call<List<ProductVO>>, t: Throwable) {
                 Toast.makeText(context, "getProductInfo_onFailure\n$t", Toast.LENGTH_SHORT).show()
 
             }
-
-            override fun onResponse(call: Call<ProductVO>, response: Response<ProductVO>) {
+            override fun onResponse(call: Call<List<ProductVO>>, response: Response<List<ProductVO>>) {
                 if(response.isSuccessful){
-                    val item : Data
-                    item = (response.body()!!.data)
-                    var items : List<Content> = item.content
+                    var items : List<ProductVO>? =  response.body()
 
                     val intent = Intent(context, ResultActivity::class.java)
                     Log.i("getProductInfo", response.body().toString())
-                    intent.putExtra("product_name",items.toString())
-                    intent.putExtra("product_confidence", items.toString())
+                    intent.putExtra("product_name", items.get(0).data.toString())
+                    intent.putExtra("product_confidence", items?.get(0)?.toString())
                     intent.flags = FLAG_ACTIVITY_SINGLE_TOP
                     startActivity(context, intent, null)
 
