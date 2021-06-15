d3b472965b206f69bbbf48bd62c878ca605033d1
Sang Hyeok Han <contea95@naver.com>
2021-06-15 12:49:02 +0900

    Add current Server file

--- Flask_server/server.py
+++ Flask_server/server.py
@@ -1,5 +1,7 @@
 from logging import debug
 import flask
+import io
+import torch
 from flask import Flask, request, render_template
 from flask.json import jsonify
 from keras.models import load_model
@@ -14,27 +16,25 @@ api = Api(app)
 
 @app.route('/API', methods=['POST', 'GET'])
 def pred():
-    if request.method == 'POST':
-        file = request.files['image']
-        if not file:
-            return jsonify({'1st': 'ERROR', '2nd': 'ERROR'})
-        # 이미지 픽셀 정보 읽기
-        img = Image.open(file)
-        img = img.convert("RGB")
-        img = img.resize((224, 224))
-        img = np.array(img)/255
-        img = img.reshape(1, 224, 224, 3)
+    if not request.method == "POST":
+        return
 
-        # 이미지 예측
-        prediction = model.predict(img)
-        label = str(np.squeeze(prediction))
-        return jsonify({'1st': label})
-    if request.method == 'GET':
-        return 'get!'
+    if request.files.get("image"):
+        image_file = request.files["image"]
+        image_bytes = image_file.read()
+
+        img = Image.open(io.BytesIO(image_bytes))
+
+        results = model(img)  # reduce size=320 for faster inference
+        print(results.pandas().xyxy[0].to_json(orient="records"))
+        return results.pandas().xyxy[0].to_json(orient="records")
 
 
 if __name__ == '__main__':
     # 모델 로드
-    model = load_model('Yolo_Practice_Repository/model/best-cnn-model.h5')
+    model = torch.hub.load(
+        "contea95/Yolo_Practice_Repository", "custom")
+    model.conf = 0.60
+    model.iou = 0.2
     # 공유기 사용 시 포트포워딩 필요
     app.run(host='0.0.0.0', port=5000, debug=True)
